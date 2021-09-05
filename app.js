f7729453cac06dd02d559c1f75923f669f946907
GabrieSotelo97 <87153830+GabrieSotelo97@users.noreply.github.com>
2021-09-04 21:11:26 -0300

    agrega login y register

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.use(express.static('public'))
 const port =3030;
 
 app.get('/home',(req,res)=> res.sendFile(path.join(__dirname,'views','home.html')))
-
+app.get('/register',(req,res)=> res.sendFile(path.join(__dirname, 'views', 'register.html')))
+app.get('/login',(req,res)=> res.sendFile(path.join(__dirname, 'views', 'login.html')))
 
 app.listen(port,()=> console.log('servidor corriendo en http://localhost:'+ port + '/home'))
